9cf42b0549fd235ed41bb41e2c784f86ca8a026c
AmosGloria <amoskesegloria@gmail.com>
2024-08-13 20:32:11 +0100

    fix: modified backend files

--- backend/database.js
+++ backend/database.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',       
   user: 'root',       
-  password: 'Kese4Glory', 
+  password: 'your_password', 
   database: 'expense_tracker', 
   waitForConnections: true,
   connectionLimit: 10,
